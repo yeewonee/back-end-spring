f616fc1aa1db83046b38298ba1bc86d75c2a631c
jeongyewon <yeewonee@naver.com>
2021-04-16 17:41:02 +0900

    커밋짱

---
+++ src/main/java/com/mycompany/webapp/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.mycompany.webapp.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HomeController {
+	private final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	@RequestMapping
+	public String home() {
+		logger.info("실행");
+		return "home";
+	}
+}
